6ef1b6266b737c893aa119efd92051b2cb7e2673
Griffin Tomaszewski <code@griffinht.com>
2025-02-09 20:22:25 -0500

    add htmx

--- src/Page.tsx
+++ src/Page.tsx
@@ -7,6 +7,7 @@ const Page: FC = (props) => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+        <script src="https://unpkg.com/htmx.org@2.0.4/dist/htmx.min.js"></script>
         <title>PayPerLess</title>
       </head>
       <body className="bg-gray-100 min-h-screen">
